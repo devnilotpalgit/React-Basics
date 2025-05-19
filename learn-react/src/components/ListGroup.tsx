d89abc700fb938170b2796d534b21dc9501c98fd
Nilotpal <nilotpaldas.uni@gmail.com>
2025-05-19 15:21:40 +0530

    State management

--- learn-react/src/components/ListGroup.tsx
+++ learn-react/src/components/ListGroup.tsx
@@ -1,15 +1,23 @@
-import { MouseEvent } from "react";
+import { useState } from "react";
+
 function ListGroup() {
   const items = ["Apple", "Mango", "Banana", "JackFruit"];
-  const handleClick = (event: MouseEvent) => {
-    console.log(event);
-  };
+  const [selectedIndex, setSelectedIndex] = useState(-1);
+
   return (
     <>
       <h2>List of Random Items</h2>
       <ul className="list-group">
-        {items.map((item) => (
-          <li className="list-group-item" key={item} onClick={handleClick}>
+        {items.map((item, index) => (
+          <li
+            className={
+              selectedIndex === index
+                ? "list-group-item active"
+                : "list-group-item"
+            }
+            key={item}
+            onClick={() => setSelectedIndex(index)}
+          >
             {item}
           </li>
         ))}
